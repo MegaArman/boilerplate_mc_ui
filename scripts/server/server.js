195fbe5879de2f4d3c7ba09cc6a3c6d64a55aee4
arman <>
2020-07-17 23:35:28 -0700

    change pack name in code

--- scripts/server/server.js
+++ scripts/server/server.js
@@ -26,7 +26,7 @@ system.onUsed = function(eventData)
 	let item = handContainer.data[0];
 	if(player.__identifier__ === "minecraft:player")
 	{
-		if (item.item === "ui_item:ui_item")
+		if (item.item === "boilerplate:ui_item")
 		{
 			let event = system.createEventData("Main:loadui");
 			event.data=eventData;
